520cc0019c5f91be6480946e426020f217ba02d8
sortedmap <sortedmap@gmail.com>
2021-05-22 17:55:22 +0300

    jquery added

--- src/main/java/main/ChatController.java
+++ src/main/java/main/ChatController.java
@@ -1,7 +1,11 @@
 package main;
 
+import main.model.Message;
 import main.model.User;
+import main.repos.MessageRepository;
 import main.repos.UserRepository;
+import main.response.AddMessageResponse;
+import main.response.AuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -18,12 +23,20 @@ public class ChatController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MessageRepository messageRepository;
+
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
+
     @GetMapping(path = "/api/auth")
-    public HashMap<String, Boolean> auth() {
-        HashMap<String, Boolean> response = new HashMap<>();
+    public AuthResponse auth() {
+        AuthResponse response = new AuthResponse();
         String sessionId = getSessionId();
         User user = userRepository.getBySessionId(sessionId);
-        response.put("result", user != null);
+        response.setResult(user != null);
+        if(user != null) {
+            response.setName(user.getName());
+        }
         return response;
     }
 
@@ -38,6 +51,26 @@ public class ChatController {
         userRepository.save(user);
         HashMap<String, Boolean> response = new HashMap<>();
         response.put("result", true);
+        return response;
+    }
+
+    @PostMapping(path = "/api/messages")
+    public AddMessageResponse addMessage(HttpServletRequest request) {
+        String text = request.getParameter("text");
+
+        String sessionId = getSessionId();
+        User user = userRepository.getBySessionId(sessionId);
+
+        Date time = new Date();
+        Message message = new Message();
+        message.setSendTime(time);
+        message.setUser(user);
+        message.setText(text);
+        messageRepository.save(message);
+
+        AddMessageResponse response = new AddMessageResponse();
+        response.setResult(true);
+        response.setTime(formatter.format(time));
         return response;
     }
 
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,10 @@
 $(function(){
 
+    var userName = 'Юзер';
+
     let initChat = function() {
         //load messages
-        //load users
-        alert('YES');
+        //loadUsers();
     };
 
     let loadUsers = function() {
@@ -20,6 +21,7 @@ $(function(){
 
     let authUser = function() {
         let name = prompt('Введите имя пользователя:');
+        userName = name;
         $.post('/api/users', {'name': name}, function(response){
             if(response.result) {
                 initChat();
@@ -32,6 +34,7 @@ $(function(){
     let checkAuthStatus = function() {
         $.get('/api/auth', function(response){
             if(response.result) {
+                userName = response.name;
                 initChat();
             } else {
                 authUser();
@@ -40,4 +43,20 @@ $(function(){
     };
 
     checkAuthStatus();
+
+    $('.send-message').on('click', function(){
+        let message = $('.message-text').val();
+        let messagesList = $('.messages-list');
+        $.post('/api/messages', {'text': message}, function(response){
+            if(response.result) {
+                let messageItem = $('<div class="message"><b>' +
+                    response.time + "&nbsp;" + userName +
+                    '</b> ' + message + '</div>');
+                messagesList.append(messageItem);
+                $('.message-text').val('');
+            } else {
+                alert('Что-то пошлло не так :(');
+            }
+        });
+    });
 });
